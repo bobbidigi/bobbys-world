58e3cf011de7cf9f95d5bfc16e34ac30b513696e
bobbidigi <bobbidigi34@gmail.com>
2017-12-13 04:56:33 -0500

    feat: weather icons and bg img and time

--- weather.js
+++ weather.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
     var units = "&units=metric"
 
     var url = api + lat + long + appid + units   
-
+    
     $.getJSON(url, function(data){
       if(data){
         var humidity = data.main.humidity;
@@ -23,12 +23,15 @@ $(document).ready(function(){
       }
       
       $("#city").html(city);
-      $("#temp").html(fahrenheit+"℉");
+      $("#temp").html(fahrenheit +" "+" ℉"+ "|"+" °C");
 //	"<img src= './images/weather.png'>"	
       $("#humidity").html("Humidity: "+humidity+"%");
       $("#windSpeed").html("Wind Speed: "+windSpeed+"m/s");
       $("#clouds").html("Clouds: " + clouds+"%");
+     
+        
       
+        
       var tempChange = true;
       $("#temp").click(function(){
         if(tempChange === true){
@@ -40,19 +43,47 @@ $(document).ready(function(){
         }
       });
       
-/*      if(celsius > 20 && clouds > 50){
-        $("body").css("background-image", "url(http://retouchthesky.com/wp-content/uploads/2014/08/IMG_4952.jpg)")   
+        
+       var weather_img = [
+        {title: "<li><img class='weather__today__image' src='images/sunny.png' alt='icon of a partially cloudy day'></li>"},
+        {title: "<li><img class='weather__today__image' src='images/weather.png' alt='icon of a partially cloudy day'></li>"},
+        {title: "<li><img class='weather__today__image' src='images/cloudy.png' alt='icon of a cloudy day'></li>"}, 
+        {title: "<li><img class='weather__today__image' src='images/rain.png' alt='icon of a cloudy day'></li>"}
+         
+        ];
+        
+      if(celsius > 20 && clouds > 50){
+        $("#weather__image").html(weather_img[0].title);  
+      } else if (clouds > 50 && humidity > 70) {
+         $("#weather__image").html(weather_img[3].title); 
       } else if (celsius > 20 && clouds < 50){
-        $("body").css("background-image", "url(https://balancebox.files.wordpress.com/2012/04/dsc_0494-edit.jpg)")   
+         $("#weather__image").html(weather_img[1].title);   
       } else if (celsius < 20 && clouds > 50){
-        $("body").css("background-image", "url(https://asianspring.files.wordpress.com/2013/05/img_2402.jpg)")   
-      } else {
-        $("body").css("background-image", "url(http://way-up-north.com/wp-content/uploads/2016/04/ice-edge-and-blue-sky.jpg)")  
-      } */
-      
-      
+        $("#weather__image").html(weather_img[2].title); 
+      }else {
+         $("#weather__image").html(weather_img[1].title); 
+      }
+        
+        
+       var currentTime = new Date();
+       hour = currentTime.getHours();
+       console.log("current Time is " + hour);
+        
+      var getImageSrc = '';
+      if (hour < 6 || hour > 20){
+          getImageSrc = 'https://78.media.tumblr.com/265482623fa3dc9c9138f9d0ad546fb9/tumblr_ovuttlPUr11v3aao4o1_500.gif';
+      }else{
+         getImageSrc = 'images/hero-bg.jpg';  
+      }    
+       $('.weather').css('background-image', 'url(' + getImageSrc + ')');
+      $('.weather').css('background-size', 'cover');  
+      $('.weather').css('repeat', 'no-repeat');    
     });
-  }); 
+  });
+    
+   
+   
+    
 function getQuote() {
     var cb = Math.round(new Date().getTime() / 1000);
     $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=" + cb, function(a) {
